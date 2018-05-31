1960b72ee0f8a86834dfd0fd7de217004e6fdb5a
Дмитрий <dmish98@mail.ru>
2018-05-31 21:06:40 +0400

    Добавьте файлы проекта.

---
+++ SignalRMvc2/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRMvc2.Startup))]
+namespace SignalRMvc2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+        }
+    }
+}
